46d64408e890ca08a79f452976028ef5a75fa9c4
Christian Talmo <talmo.christian@gmail.com>
2018-09-27 22:59:09 +0300

    linted with spacestandard

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-var quickshot = require('./quickshot.js');
-var express = require('express');
-var Url = require('url');
+var quickshot = require( './quickshot.js' )
+var express = require( 'express' )
+var Url = require( 'url' )
 
 // configure express
-var app = express();
+var app = express()
 
 // query string
-app.get('/', function (req, res) {
-  var url = Url.parse(req.url, true, true);
-  var href = url.query.url;
+app.get( '/', function ( req, res ) {
+  var url = Url.parse( req.url, true, true )
+  var href = url.query.url
   // parse urls inside quotes
-  if (href[0] === '"' && href[href.length-1] === '"') {
-    href = href.substring(1, href.length-1);
+  if ( href[ 0 ] === '"' && href[ href.length - 1 ] === '"' ) {
+    href = href.substring( 1, href.length - 1 )
   }
-  console.log("href: " + href);
-  if (href && typeof href === 'string') {
-    quickshot.get(href, function (err, data) {
-      if (err) {
-        return res.send("error: " + err.message || err);
+  console.log( 'href: ' + href )
+  if ( href && typeof href === 'string' ) {
+    quickshot.get( href, function ( err, data ) {
+      if ( err ) {
+        return res.send( 'error: ' + err.message || err )
       }
-      return res.end(data, 'binary');
-    });
+      return res.end( data, 'binary' )
+    } )
   } else {
-    res.status(404).json({message: "invalid or null 'url' in querystring"}).end();
+    res.status( 404 ).json( { message: "invalid or null 'url' in querystring" } ).end()
   }
-});
+} )
 
-var port = process.env.PORT || 30999;
-var server = app.listen(port, function () {
-  var h = server.address().address;
-  var p = server.address().port;
-  console.log("QuickShot Server running at http://%s:%s", h, p);
-});
+var port = process.env.PORT || 30999
+var server = app.listen( port, function () {
+  var h = server.address().address
+  var p = server.address().port
+  console.log( 'QuickShot Server running at http://%s:%s', h, p )
+} )
--- quickshot.js
+++ quickshot.js
@@ -1,10 +1,9 @@
-var http = require('http');
-var Url = require('url');
-var webshot = require('webshot');
+var Url = require( 'url' )
+var webshot = require( 'webshot' )
 
-var Jimp = require('jimp')
+var Jimp = require( 'jimp' )
 
-var webshot_settings = {
+var webshotSettings = { // eslint-disable-line
   screenSize: {
     width: 1920,
     height: 1080
@@ -16,105 +15,107 @@ var webshot_settings = {
   }
 }
 
-var cache = {};
-var cacheList = [];
-var cacheLimit = 100;
-var listenersLimit = 300;
-var cacheTimeoutInterval = 10000; // 10 seconds
+var cache = {}
+var cacheList = []
+var cacheLimit = 100
+var listenersLimit = 300
+var cacheTimeoutInterval = 10000 // 10 seconds
 
-String.prototype.startsWith = function (str) {
-  return this.indexOf(str) === 0;
+String.prototype.startsWith = function ( str ) { // eslint-disable-line
+  return this.indexOf( str ) === 0
 }
 
-function handleError (cash, err) {
-  if (!cash)
-    return;
-  if (cash.listeners instanceof Array)
-    cash.listeners.map(function (val, ind, arr) {
-      val(err);
-    });
-  cache[cash.hostpath] = null;
+function handleError ( cash, err ) {
+  if ( !cash )
+  { return }
+  if ( cash.listeners instanceof Array )
+  { cash.listeners.map( function ( val, ind, arr ) {
+    val( err )
+  } ) }
+  cache[ cash.hostpath ] = null
 }
 
-function get (url, callback) {
-
-  if (!url.startsWith('http') && !url.startsWith('//')) {
-    url = '//' + url;
+function get ( url, callback ) {
+  if ( !url.startsWith( 'http' ) && !url.startsWith( '//' ) ) {
+    url = '//' + url
   }
 
-  var url = Url.parse(url, true, true);
+  url = Url.parse( url, true, true )
 
-  var hostpath = (url.host + (url.path || "/"));
+  var hostpath = ( url.host + ( url.path || '/' ) )
 
-  var cash = cache[hostpath];
+  var cash = cache[ hostpath ]
 
-  if ( cache ) cache.data = null // TODO remove this line
+  // if ( cache ) cache.data = null // TODO remove this line
 
-  if (cash && cash.data) { // url is cached
-    console.log("responding from cache to: " + hostpath);
-    return callback(null, cash.data);
+  if ( cash && cash.data ) { // url is cached
+    console.log( 'responding from cache to: ' + hostpath )
+    return callback( null, cash.data )
   } else { // url not cached
-    if (cash) {
+    if ( cash ) {
       // add listeners
-      if (cash.listeners.length > listenersLimit) {
+      if ( cash.listeners.length > listenersLimit ) {
         // dont add over listener capacity
-        return console.log("listener limit reached from: " + hostpath);
+        return console.log( 'listener limit reached from: ' + hostpath )
       }
-      console.log("adding a pending listener to: " + hostpath);
-      return cash.listeners.push(callback);
+      console.log( 'adding a pending listener to: ' + hostpath )
+      return cash.listeners.push( callback )
     } else {
-      var cash = cache[hostpath] = {
+      cash = cache[ hostpath ] = {
         hostpath: hostpath,
         url: url,
         data: null,
-        listeners: [callback]
-      };
+        listeners: [ callback ]
+      }
 
       // run webshot
-      console.log("Webshooting: " + hostpath);
+      console.log( 'Webshooting: ' + hostpath )
 
-      var settings = webshot_settings;
-      cacheList.push(cash);
-      return webshot(hostpath, settings, function (err, renderStream) {
-        if (err) {
+      cacheList.push( cash )
+      return webshot( hostpath, webshotSettings, function ( err, renderStream ) {
+        if ( err ) {
           // kill the litener callbacks and reset the cache
-          handleError(err, cash);
+          handleError( err, cash )
         } else {
-          var buffer = "";
+          var buffer = ''
 
           // gather the data
-          renderStream.on('data', function (data) {
-            buffer += data.toString('binary');
-          });
+          renderStream.on( 'data', function ( data ) {
+            buffer += data.toString( 'binary' )
+          } )
 
           // respond to all pending callbacks and update the cache with data
-          renderStream.on('end', function () {
+          renderStream.on( 'end', function () {
             console.log( 'jimp read' )
             Jimp.read( Buffer.from( buffer, 'binary' ) )
             .then( function ( image ) {
               console.log( 'jimp then' )
 
-              if (!cash) {
-                return console.log("error on finished webshot - no cash");
+              if ( !cash ) {
+                return console.log( 'error on finished webshot - no cash' )
               }
 
               console.log(
-                  "responding on finished webshot to: %s for %s listeners",
-                  cash.hostpath, cash.listeners.length
-                  );
+                'responding on finished webshot to: %s for %s listeners',
+                cash.hostpath, cash.listeners.length
+              )
 
               image
               .contain( 192, 108 )
               .getBuffer( Jimp.MIME_JPEG, function ( err, imageBuffer ) {
-                if (cash.listeners instanceof Array) {
-                  cash.listeners.forEach(function (val, ind, arr) {
-                    if (typeof(val) == "function")
-                      val(null, imageBuffer);
-                  });
+                if ( err ) {
+                  return handleError( err, cash )
                 }
 
-                cash.data = imageBuffer;
-                cash.listeners.length = 0;
+                if ( cash.listeners instanceof Array ) {
+                  cash.listeners.forEach( function ( val, ind, arr ) {
+                    if ( typeof ( val ) === 'function' )
+                    { val( null, imageBuffer ) }
+                  } )
+                }
+
+                cash.data = imageBuffer
+                cash.listeners.length = 0
               } )
             } )
             .catch( function ( err ) {
@@ -122,48 +123,48 @@ function get (url, callback) {
 
               handleError( err, cash )
             } )
-          });
+          } )
 
           // kill the pending callbacks and reset the cache
-          renderStream.on('error', function (err) {
-            return handleError(err, cash);
-          });
+          renderStream.on( 'error', function ( err ) {
+            return handleError( err, cash )
+          } )
         }
-      });
+      } )
     }
   }
 }
 
 // clean cache every 10 seconds
-var cacheTimeout = null;
+var cacheTimeout = null
 function cleanCache () {
-  if (cacheTimeout) {
-    clearTimeout(cacheTimeout);
-    cacheTimeout = null;
+  if ( cacheTimeout ) {
+    clearTimeout( cacheTimeout )
+    cacheTimeout = null
   }
 
   // clean cache
-  if (cacheList.length > cacheLimit) {
-    var removeList = cacheList.slice(cacheLimit);
-    for (var i = 0; i < removeList.length; i++) {
-      var cash = removeList[i];
-      if (!cash) continue;
-      if (cash.listeners instanceof Array)
-        cash.listeners.map(function (val, ind, arr) {
-          if (typeof(val) == "function")
-            val("Over capacity");
-        });
-      cache[cash.hostpath] = null;
+  if ( cacheList.length > cacheLimit ) {
+    var removeList = cacheList.slice( cacheLimit )
+    for ( var i = 0; i < removeList.length; i++ ) {
+      var cash = removeList[ i ]
+      if ( !cash ) continue
+      if ( cash.listeners instanceof Array )
+      { cash.listeners.map( function ( val, ind, arr ) {
+        if ( typeof ( val ) === 'function' )
+        { val( 'Over capacity' ) }
+      } ) }
+      cache[ cash.hostpath ] = null
     }
-    console.log("%s items removed from cache", removeList.length);
-    cacheList.length = cacheLimit;
+    console.log( '%s items removed from cache', removeList.length )
+    cacheList.length = cacheLimit
   }
 
-  console.log("cacheTimeout rewinded. cache size: [%s/%s]",
-              cacheList.length, cacheLimit);
-  cacheTimeout = setTimeout(cleanCache, cacheTimeoutInterval);
+  console.log( 'cacheTimeout rewinded. cache size: [%s/%s]',
+    cacheList.length, cacheLimit )
+  cacheTimeout = setTimeout( cleanCache, cacheTimeoutInterval )
 }
-cleanCache();
+cleanCache()
 
 module.exports = {
   get: get
